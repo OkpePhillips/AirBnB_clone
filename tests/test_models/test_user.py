213a664b917fd419dc41473385decec105066942
Godwin Okpe <okpegodwin18@yahoo.com>
2023-08-14 04:26:50 -0700

    Fixing checker 10 of task 2

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -11,6 +11,45 @@ class TestUser(unittest.TestCase):
     """
     Test class for the user attributes.
     """
+    def setUp(self):
+        """
+        Sets a general instance for succeeding testing purposes.
+        """
+
+        self.main_inst = User()
+
+        self.main_inst.email = "example@mailservice.com"
+        self.main_inst.password = "Admin"
+        self.main_inst.first_name = "David"
+        self.main_inst.last_name = "Malan"
+
+    def test_correct_store(self):
+        """
+        Tests the dictionary created before the JSON dump is made.
+        Checks if class specific variables are stored.
+        """
+
+        test_dict = self.main_inst.to_dict()
+
+        self.assertIn("email", test_dict)
+        self.assertIn("password", test_dict)
+        self.assertIn("first_name", test_dict)
+        self.assertIn("last_name", test_dict)
+
+    def test_successful_reload(self):
+        """
+        Tests for the successful reload of class specific variables
+        effectively estoring an object by cloning its data.
+        Instantiation uses the main_inst dictionary.
+        """
+
+        main_inst_dict = self.main_inst.to_dict()
+
+        self.assertIn("email", main_inst_dict)
+        self.assertIn("password", main_inst_dict)
+        self.assertIn("first_name", main_inst_dict)
+        self.assertIn("last_name", main_inst_dict)
+
     def test_user_email(self):
         """
         Checks that email attribute is properly set.
